594ef6c88cd4ac9a02895ccb6e426084111c4d16
alu10711862 <alurod1862@ieselcaminas.org>
2023-12-21 19:13:04 +0100

    Revert "no funciona"
    This reverts commit 19f1bb5b

--- app/src/main/java/com/example/pokedex/ui/viewmodels/PokedexViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/viewmodels/PokedexViewModel.kt
@@ -6,10 +6,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.pokedex.data.api.RetrofitClient
 import com.example.pokedex.data.models.Pokemon
 import com.example.pokedex.data.models.PokemonDeserializer
 import com.example.pokedex.data.models.PokemonType
 import com.google.gson.GsonBuilder
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 
@@ -18,16 +21,31 @@ class PokedexViewModel(application: Application) : AndroidViewModel(application)
     private val _pokemonLiveData = MutableLiveData<Pokemon>()
     val pokemonLiveData: LiveData<Pokemon> get() = _pokemonLiveData
     val appContext = getApplication<Application>().applicationContext
+    private val pokemonApiService = RetrofitClient.pokemonApiService
+
 
 
     init {
-        loadPokemon(pokemonName = pokemonName("arcanine"))
+        //loadPokemon(pokemonName = pokemonName("zekrom"))
+        loadPokemonApi(pokemonName = "charizard")
+
+    }
+
+
+    private fun loadPokemonApi(pokemonName: String) {
+        viewModelScope.launch {
+            try {
+                val pokemon = pokemonApiService.getPokemon(pokemonName)
+                _pokemonLiveData.value = pokemon
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
 
     private fun loadPokemon(pokemonName: String) {
-        val gson =
-            GsonBuilder().registerTypeAdapter(Pokemon::class.java, PokemonDeserializer()).create()
+        val gson = GsonBuilder().registerTypeAdapter(Pokemon::class.java, PokemonDeserializer()).create()
         val pokemon = gson.fromJson(getPokemon(appContext, pokemonName), Pokemon::class.java)
         println(_pokemonLiveData.value?.name)
         _pokemonLiveData.value = pokemon
